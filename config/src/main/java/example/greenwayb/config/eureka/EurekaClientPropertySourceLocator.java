fead4f22a3682936eff1f4898e06cee61ec55835
greenwayb <GreenwBe@cba.com.au>
2019-05-15 09:23:28 +0800

    Use EnvironmentPostProcessor to resolve the issues of securityproperties being loaded in enough time for DiscoveryClient to work with security.  Thanks Dave Syer!

--- config/src/main/java/example/greenwayb/config/eureka/EurekaClientPropertySourceLocator.java
+++ config/src/main/java/example/greenwayb/config/eureka/EurekaClientPropertySourceLocator.java
@@ -1,18 +1,18 @@
 package example.greenwayb.config.eureka;
 
 import example.greenwayb.bootstrap.ConfigurationLoader;
-import example.greenwayb.bootstrap.PropertySourceLocator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
-import org.springframework.core.env.Environment;
-import org.springframework.core.env.PropertySource;
 
 /**
- *
+ * Use EnvironmentPostProcessor and the issue with cloud config via DiscoveryClient and security progperties not being loaded goes away
  */
-public class EurekaClientPropertySourceLocator implements PropertySourceLocator {
+public class EurekaClientPropertySourceLocator implements EnvironmentPostProcessor {
 
     @Override
-    public PropertySource<?> locate(Environment environment) {
-        return ConfigurationLoader.getPropertySource((ConfigurableEnvironment)environment, "eurekaClient", "eureka-client");
+    public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
+        environment.getPropertySources().addLast(ConfigurationLoader.getPropertySource(environment, "eurekaClient", "eureka-client"));
     }
+
 }
